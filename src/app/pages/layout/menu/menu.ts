cf993541129aeef998f6ebf5e5f0d16ec0ef0b6e
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-06-13 18:47:28 -0300

    feat: update dependencies and add Tailwind CSS integration
    - Added @angular/cdk, @ng-icons/core, @ng-icons/lucide, @spartan-ng/brain, @tailwindcss/postcss, class-variance-authority, clsx, postcss, tailwindcss, tailwind-merge, and tailwindcss-animate to dependencies.
    - Updated menu component to include a sidebar with toggle functionality.
    - Refactored menu component to use standalone Angular components and added necessary imports.
    - Enhanced global styles with Tailwind CSS and custom CSS variables for theming.
    - Configured TypeScript paths for Spartan NG Helm button and icon modules.
    - Created PostCSS configuration for Tailwind CSS.
    - Established components path for UI libraries.
    - Implemented HlmButtonModule and HlmIconModule with directives for button and icon functionalities.
    - Defined button and icon configuration tokens for customizable properties.

--- src/app/pages/layout/menu/menu.ts
+++ src/app/pages/layout/menu/menu.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-
+import { HlmButtonModule } from '@spartan-ng/helm/button';
+import { HlmIconDirective } from '@spartan-ng/helm/icon';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucidePanelRightClose, lucidePanelLeftClose } from '@ng-icons/lucide';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-menu',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    HlmButtonModule,
+    HlmIconDirective,
+    NgIcon,
+  ],
+  providers: [provideIcons({ lucidePanelRightClose, lucidePanelLeftClose })],
   templateUrl: './menu.html',
-  styleUrl: './menu.scss'
+  styleUrl: './menu.scss',
 })
 export class Menu {
+  sidebarOpen = false;
 
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
 }
