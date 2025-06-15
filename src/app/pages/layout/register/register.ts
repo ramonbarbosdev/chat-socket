f76d38100dab45e7c959e4baa24865bb5447dbf4
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-06-15 16:17:56 -0300

    feat: refatorar página de registro para usar componente app-input e adicionar feedback de sucesso no cadastro

--- src/app/pages/layout/register/register.ts
+++ src/app/pages/layout/register/register.ts
@@ -1,15 +1,51 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { HlmFormFieldModule } from '@spartan-ng/helm/form-field';
 import { HlmInputDirective } from '@spartan-ng/helm/input';
 import { HlmButtonDirective } from '@spartan-ng/helm/button';
+import { InputCustom } from '../../../components/input/input';
+import { AuthService } from '../../../auth/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
-  imports: [HlmInputDirective,HlmFormFieldModule, RouterLink,HlmButtonDirective  ],
+  imports: [
+    HlmInputDirective,
+    HlmFormFieldModule,
+    RouterLink,
+    HlmButtonDirective,
+    InputCustom,
+  ],
   templateUrl: './register.html',
-  styleUrl: './register.scss'
+  styleUrl: './register.scss',
 })
 export class Register {
+  public objeto = {
+    login: '',
+    nome: '',
+    senha: '',
+    token: '',
+  };
 
+  auth = inject(AuthService);
+
+  router = inject(Router);
+
+  cadastrar() {
+    this.auth.cadastrar(this.objeto).subscribe({
+      next: (res: any) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Cadastro realizado com sucesso!',
+          text: 'Você pode fazer login agora.',
+          confirmButtonText: 'OK',
+        });
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        console.log(err);
+        
+      },
+    });
+  }
 }
