59e7a39842342ccce04d90d685c3000fc338188f
Ramon Barbosa Souza <ramon.barbosa@w5i.com.br>
2025-06-27 16:51:04 -0300

    feat: atualizar URLs da API para o ambiente de produção

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/chatsocket',
-  // apiUrl: 'https://api-chat-socket.onrender.com/chatsocket',
-  apiUrlWebSocket: '//localhost:8080/chatsocket/chat-socket',
-  // apiUrlWebSocket:
-    // 'https://api-chat-socket.onrender.com/chatsocket/chat-socket',
+  // apiUrl: 'http://localhost:8080/chatsocket',
+  apiUrl: 'https://api-chat-socket.onrender.com/chatsocket',
+  // apiUrlWebSocket: '//localhost:8080/chatsocket/chat-socket',
+  apiUrlWebSocket:
+    'https://api-chat-socket.onrender.com/chatsocket/chat-socket',
 };
